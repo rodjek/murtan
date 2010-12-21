3111e48c2eadbcc684f229a2ca9e184c059f6c75
Tim Sharpe <tim@github.com>
2010-12-21 15:38:42 +1300

    Actually add the file containing the test suite

---
+++ test/filter.rb
@@ -0,0 +1,12 @@
+class Murtan_TestFilter < Test::Unit::TestCase
+  def test_filter_direction_and_interface_only
+    data = Parser.new.parse("pass in on lo")
+    assert_equal ["-A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT",
+        "-A INPUT -i lo -m state --state NEW -J ACCEPT"], data.to_iptables
+  end
+#    pass in on lo
+#    pass in log on { eth0 eth2 } proto { tcp icmp } from { 1.2.3.4/24 } port { 22 21 } to port 22
+#    block drop out on $bar proto { tcp udp } from 10.0.0.1 port { 80 443 } no state
+#    block in proto tcp from any to any port 22
+#    block in
+end
