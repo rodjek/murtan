eefbdd3c978433542bf01563c7df5e594b956281
Tim Sharpe <tim@sharpe.id.au>
2011-05-28 09:21:44 +1000

    Parse IP addresses

--- lib/murtan/parser.rb
+++ lib/murtan/parser.rb
@@ -2,5 +2,15 @@ require 'parslet'
 
 module Murtan
   class Parser < Parslet::Parser
+    rule(:integer)            { match('[0-9]') }
+    rule(:period)             { str('.') }
+    rule(:slash)              { str('/') }
+    rule(:ip)                 { integer.repeat(1, 3) >> period >>
+                                integer.repeat(1, 3) >> period >>
+                                integer.repeat(1, 3) >> period >>
+                                integer.repeat(1, 3) }
+    rule(:netmask_or_masklen) { slash >> ip.as(:netmask) |
+                                slash >> integer.repeat(1).as(:masklen) }
+    rule(:address)            { ip.as(:ip) >> netmask_or_masklen.maybe }
   end
 end
--- spec/murtan/parser_spec.rb
+++ spec/murtan/parser_spec.rb
@@ -4,10 +4,12 @@ require 'parslet/rig/rspec'
 describe Murtan::Parser do
   let(:parser) { described_class.new }
 
-  context "address" do
+  context 'address' do
     subject { parser.address }
 
-    it { should parse("1.2.3.4").
-      as(:first => '1', :second => '2', :third => '3', :fourth => '4') }
+    it { should parse('1.2.3.4').as(:ip => '1.2.3.4') }
+    it { should parse('1.2.3.0/24').as(:ip => '1.2.3.0', :masklen => '24') }
+    it { should parse('1.2.3.0/255.255.255.0').as(
+      :ip => '1.2.3.0', :netmask => '255.255.255.0')}
   end
 end
