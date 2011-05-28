457626b45227fb78f0549f43fffe12029e4de30b
Tim Sharpe <tim@sharpe.id.au>
2011-05-28 10:45:05 +1000

    Clean up netmask_or_masklen

--- lib/murtan/parser.rb
+++ lib/murtan/parser.rb
@@ -16,8 +16,8 @@ module Murtan
                                 integer.repeat(1, 3) >> period >>
                                 integer.repeat(1, 3) >> period >>
                                 integer.repeat(1, 3) }
-    rule(:netmask_or_masklen) { slash >> ip.as(:netmask) |
-                                slash >> integer.repeat(1).as(:masklen) }
+    rule(:netmask_or_masklen) { slash >> (ip.as(:netmask) |
+                                integer.repeat(1).as(:masklen)) }
     rule(:address)            { ip.as(:ip) >> netmask_or_masklen.maybe }
 
     #########################################################################
